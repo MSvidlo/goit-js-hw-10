38ee7a0d569dbc6b8eae47c1e3ecb1d2663e332e
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-02-06 22:06:43 +0000

    Deploying to gh-pages from @ MSvidlo/goit-js-hw-10@a79ffebfb8d8e1ca43830d69f3a4994c7c6de4e3 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";const e=document.querySelector(".form");e.addEventListener("submit",function(t){t.preventDefault()});
+//# sourceMappingURL=commonHelpers2.js.map
