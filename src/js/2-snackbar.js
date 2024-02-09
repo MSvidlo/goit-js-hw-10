4efbdb892c227668b31e3b5d8c1630351c9b51db
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-02-09 22:46:11 +0100

    done second task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,6 +5,47 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const myForm = document.querySelector('.form');
 myForm.addEventListener('submit', function (e) {
-    e.preventDefault()
+    e.preventDefault();
+    const selectedOption= document.querySelector('input[name="state"]:checked').value
+    const delay = document.querySelector('.indelay').value;
 
-})
+
+     const inputElement = document.querySelector('input[type="radio"]');
+    
+    
+    // Створюємо проміс
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (selectedOption === "fulfilled") {
+                resolve(delay)
+            } else if (selectedOption === "rejected") {
+                reject(delay)
+            }
+        }, delay);
+        
+    });
+
+
+
+          promise.then((delay) => {
+        iziToast.success({
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topCenter',
+            backgroundColor: '#59a10d',
+            color: '#fff',
+            theme: 'dark',
+            maxWidth: '383px',
+            
+        });
+    }).catch((delay) => {
+        iziToast.error({
+           message: `❌ Rejected promise in ${delay}ms`,
+            position: 'topCenter',
+            backgroundColor: '#ef4040',
+            color: '#fff',
+            theme: 'dark',
+            maxWidth: '302px',
+        
+        });
+    });
+});
